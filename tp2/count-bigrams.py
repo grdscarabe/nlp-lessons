0ed72811246d0c4175a5a8bfcae4ff14e092e485
Fabien Poulard <fpoulard@dictanova.com>
2012-11-23 13:28:43 +0100

    Avancement dans la rédaction de l'énoncé

---
+++ tp2/count-bigrams.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Ce script permet de compter les bigrammes caractères présents dans des textes
+encodés en UTF-8 et d'exporter le résultat de ce comptage au format CSV.
+
+Exemple d'utilisation :
+    python count-bigrams.py texte.txt resultat.csv
+"""
+
+import codecs, sys
+
+# Lecture du fichier en entrée
+finput = codecs.open(sys.argv[1], "r", "utf-8")
+content = finput.read()
+finput.close()
+
+# Normalisation des sauts de ligne
+content = content.replace("\r\n", " ").replace("\n\r", " ").replace("\n", " ").replace("\r", " ")
+
+# Comptage des bigrammes
+bigrams = {}
+for i in range(1, len(content)):
+	bigram = content[i-1:i+1]
+	if not bigrams.has_key(bigram):
+		bigrams[bigram] = 0
+	bigrams[bigram] += 1
+
+# Exportation au format csv
+fcsv = codecs.open(sys.argv[2], "w", "utf-8")
+for bigram,occ in bigrams.items():
+	fcsv.write("\"%s\",%d\n" % (bigram.replace("\"", "<quote>"),occ))
+fcsv.close()
+
+
